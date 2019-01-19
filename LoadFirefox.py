75797b3e366c9cc6a61a7f3ebc3a97267c773e64
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2019-01-19 02:09:13 -0600

    adding class to seo of notices

--- LoadFirefox.py
+++ LoadFirefox.py
@@ -7,6 +7,7 @@ class LoadFirefox:
     """ Load driver and browser in Firefox """
     def __init__(self, driver):
         """ Init of class, initializes the browser of webdriver in the variable browser """
+        self.driver = driver
         self.browser = self._return_browser(driver)
 
     def __del__(self):
@@ -47,23 +48,74 @@ class LoadFirefox:
             marks.append(', '.join(a_list))
         #print(marks)
         #print(len(marks))
-        df = pd.Series(marks, index=list(range(len(marks))))
         #print(df)
-        return df
+        return pd.Series(marks, index=list(range(len(marks))))
 
-    def get_trending_daily():
+    def get_trending_daily(self, geo='US'):
         """ Send the GET request to trending realtime """
-        (self.browser.get('https://trends.google.com.mx/trends/trendingsearches/realtime?geo='
-        +str(geo)+'&category='+str(cat))) #this is the same function lol
-        soup = bf(self.browser.page_source, 'html.parser')
+        (self.browser.get('https://trends.google.com/trends/trendingsearches/daily?geo='+
+        geo)) #this is the same function lol
+        details_top = self.browser.find_elements_by_class_name('details-top')
+        count_title = self.browser.find_elements_by_class_name('search-count-title')
+        summary_text = self.browser.find_elements_by_class_name('summary-text')
+        #print(details_top)
+        a = [i.find_element_by_tag_name('a').text for i in details_top]
+        b = [j.text for j in count_title]
+        links_news = [k.find_element_by_tag_name('a').get_attribute('href') for k in summary_text]
+        c = _NewsSeo(links_news[0], self.driver)
+        print(a)
+        print()
+        print(b)
+        print()
+        print(c.metas)
+
 
     def no_mames(self):
         """ This is a test function :v """
         print('no_mames')
 
+class _NewsSeo():
+    """ News SEO information (meta tags) """
+    def __init__(self, link, driver):
+        self.link = link
+        self.browser = self._return_browser(driver)
+        self.metas = self._get_meta()
+
+    def __del__(self):
+        """ Called on garbage recolection, destroy the webdriver window, it can be stoped with an input() """
+        self.browser.close()
+
+    def _return_browser(self, driver):
+        """ Return the browser of webdriver """
+        try:
+            print(driver)
+            #opti = Options()
+            #opti.set_headless(headless=True)
+            br_path = str(driver)
+            #browser = webdriver.Firefox(firefox_options=opti, executable_path=br_path)
+            browser = webdriver.Firefox(executable_path=br_path)
+            print('Headless Browser Invoked!')
+            browser.implicitly_wait(10) #seconds
+            return browser
+        except:
+            print('Exception ocurred when Initializing the browser')
+            browser.close()
+
+    def _get_meta(self):
+        self.browser.get(self.link)
+        metas = self.browser.find_elements_by_tag_name('meta')
+        metas_i = []
+        for i in metas:
+            try:
+                metas_i.append(i.get_attribute('name'))
+            except:
+                continue
+        print(metas_i)
+        return metas
+
 if(__name__=='__main__'):
     browser = LoadFirefox('/home/jose/Dropbox/Drivers/geckodriver')
     browser.no_mames()
     data = browser.get_trending_realtime()
-    data.to_csv('../prueba.csv')
+    browser.get_trending_daily()
     input('Press any key')
